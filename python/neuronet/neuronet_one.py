3c63ae30a707bfafa09cf7ffc23f1868645086a9
AlexeyKopytko <stranik1401@gmail.com>
2022-02-04 07:26:45 +0300

    s3

--- python/neuronet/neuronet_one.py
+++ python/neuronet/neuronet_one.py
@@ -21,10 +21,12 @@ y_train = np.array(data_education_np[:, 32:]).astype(float)
 x_test = np.array(data_test_np[:, :32])
 y_test = np.array(data_test_np[:, 32:]).astype(float)
 
-x_train_adv = np.array()
 
-for x in x_train:
-    line = np.array(x)
+x_train_adv = np.empty([2, 960])
+
+for x in range(len(x_train)):
+    line = x_train[x]
+    r = np.array([-1])
     for y in line:
         st = str(y)
         h = 0
@@ -37,22 +39,54 @@ for x in x_train:
         h_cat = keras.utils.to_categorical(h, 10)
         t_cat = keras.utils.to_categorical(t, 10)
         o_cat = keras.utils.to_categorical(o, 10)
-        result =
+        result = np.append(h_cat, t_cat)
+        result = np.append(result, o_cat)
+        r = np.append(r, result)
+    r = np.delete(r, 0)
+    r = np.expand_dims(r, axis = 0)
+    x_train_adv = np.concatenate((x_train_adv, r), axis=0)
+    print(x)
 
+x_train_adv = np.delete(x_train_adv, 0, 0)
+x_train_adv = np.delete(x_train_adv, 0, 0)
 
+x_test_adv = np.empty([2, 960])
 
+for x in range(len(x_test)):
+    line = x_test[x]
+    r = np.array([-1])
+    for y in line:
+        st = str(y)
+        h = 0
+        t = 0
+        o = int(st[-1])
+        if len(st) >= 2:
+            t = int(st[-2])
+        if len(st) >= 3:
+           h = int(st[-3])
+        h_cat = keras.utils.to_categorical(h, 10)
+        t_cat = keras.utils.to_categorical(t, 10)
+        o_cat = keras.utils.to_categorical(o, 10)
+        result = np.append(h_cat, t_cat)
+        result = np.append(result, o_cat)
+        r = np.append(r, result)
+    r = np.delete(r, 0)
+    r = np.expand_dims(r, axis = 0)
+    x_test_adv = np.concatenate((x_test_adv, r), axis=0)
+    print(x)
+
+x_test_adv = np.delete(x_test_adv, 0, 0)
+x_test_adv = np.delete(x_test_adv, 0, 0)
 
 #x_train = x_train / 999
 #x_test = x_test / 999
 
-y_train_cat = keras.utils.to_categorical(y_train, 2)
-y_test_cat = keras.utils.to_categorical(y_test, 2)
+y_train_cat = keras.utils.to_categorical(y_train, 3)
+y_test_cat = keras.utils.to_categorical(y_test, 3)
 
 model = keras.Sequential([
-    Dense(units=103, input_shape=(32,), activation='relu'),
-    Dense(units=309, input_shape=(103,), activation='relu'),
-    Dense(units=927, input_shape=(309,), activation='relu'),
-    Dense(units=20, activation='softmax'),
+    Dense(units=90, input_shape=(960,), activation='relu'),
+    Dense(units=3, activation='softmax'),
 ])
 
 print(model.summary())      # вывод структуры НС в консоль
@@ -62,9 +96,9 @@ model.compile(optimizer='adam',
              metrics=['accuracy'])
 
 
-model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
+model.fit(x_train_adv, y_train_cat, batch_size=32, epochs=50, validation_split=0.2)
 
-model.evaluate(x_test, y_test)
+model.evaluate(x_test_adv, y_test_cat)
 
 
 
